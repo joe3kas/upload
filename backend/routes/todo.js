0f352885f5be14386a229ff4fda20651f2029ff9
joe bocanegra c <84000797+joe3kas@users.noreply.github.com>
2021-10-21 14:44:40 -0500

    joe3kas
    esta es al fin

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -7,12 +7,14 @@ const { unlink } = require('fs-extra');
 const Slider = require('../models/Slider');
 const Docente = require('../models/Docente');
 const Noticia = require('../models/Noticia');
+const Cuadro = require('../models/Cuadro');
 
 router.get('/', async (req, res) => {
     const sliders = await Slider.find().sort('-_id');
     const docentes = await Docente.find().sort('-_id');
     const noticias = await Noticia.find().sort('-_id');
-    res.json({sliders,noticias,docentes});
+    const honors = await Cuadro.find().sort('-_id');
+    res.json({sliders,noticias,docentes,honors});
 });
 
 module.exports = router;
